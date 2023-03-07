d4aad20bc4ef0dfd9c5f86ddd1ffe412dbd9f8d9
babai.david <davic0444@gmail.com>
2023-03-07 21:21:04 +0100

    Improve pagination

--- frontend/src/components/PaginationComponent.js
+++ frontend/src/components/PaginationComponent.js
@@ -1,25 +1,21 @@
-import {Pagination} from "react-bootstrap";
+import { Pagination } from "react-bootstrap";
 
 const PaginationComponent = () => {
-    return (
-        <Pagination>
-            <Pagination.First/>
-            <Pagination.Prev/>
-            <Pagination.Item>{1}</Pagination.Item>
-            <Pagination.Ellipsis/>
+        return (
+            <Pagination>
+                    <Pagination.Prev />
+                    <Pagination.Item>{1}</Pagination.Item>
 
-            <Pagination.Item>{10}</Pagination.Item>
-            <Pagination.Item>{11}</Pagination.Item>
-            <Pagination.Item active>{12}</Pagination.Item>
-            <Pagination.Item>{13}</Pagination.Item>
-            <Pagination.Item disabled>{14}</Pagination.Item>
+                    <Pagination.Item>{10}</Pagination.Item>
+                    <Pagination.Item>{11}</Pagination.Item>
+                    <Pagination.Item active>{12}</Pagination.Item>
+                    <Pagination.Item>{13}</Pagination.Item>
+                    <Pagination.Item>{14}</Pagination.Item>
 
-            <Pagination.Ellipsis/>
-            <Pagination.Item>{20}</Pagination.Item>
-            <Pagination.Next/>
-            <Pagination.Last/>
-        </Pagination>
-    );
+                    <Pagination.Item>{20}</Pagination.Item>
+                    <Pagination.Next />
+            </Pagination>
+        );
 };
 
 export default PaginationComponent;
